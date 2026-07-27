e353513fb154b463cca11a9908a84b8b37661d0e
Soumya Nandi <soumyanandi2003@gmail.com>
2026-07-27 17:51:37 +0530

    Seeting up the server

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv= require("dotenv");
+const cors = require ("cors")
+const connectDB = require("./src/config/db")
+
+
+
+dotenv.config();
+
+//database connection
+connectDB();
+
+const app = express();
+
+app.use(cors({
+    origin:process.env.CLIENT_URL,
+    credentials:true
+}));
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log("Server is running on ", PORT);
+    
+});
