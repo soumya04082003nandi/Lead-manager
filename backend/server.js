cfd7e9b1e6c13f04d22e8e06073f949bd7cbafef
Soumya Nandi <soumyanandi2003@gmail.com>
2026-07-27 18:46:40 +0530

    Using cookieparser

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const dotenv= require("dotenv");
-const cors = require ("cors")
+const dotenv = require("dotenv");
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
 const connectDB = require("./src/config/db")
 
 
@@ -13,19 +14,21 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin:process.env.CLIENT_URL,
-    credentials:true
+    origin: process.env.CLIENT_URL,
+    credentials: true
 }));
+
+app.use(cookieParser());
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("API is running");
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
-    
+
 });
