418b2757a077aab1e54c50cce16f9cb834e7e4bd
Soumya Nandi <soumyanandi2003@gmail.com>
2026-07-27 18:37:58 +0530

    Creating the role checking middleware

---
+++ backend/src/middleware/role.js
@@ -0,0 +1,11 @@
+const checkRole= (...role)=>{
+    return (req,res,next)=>{
+        if(!role.includes(req.user.role))
+            return res.status(403).json({
+                message:"Forbidden"
+            });
+        next();
+    };
+};
+
+module.exports=checkRole;
