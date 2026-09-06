225eb3997de588b69ac550b8a4b77d4a5a1a44b0
Soumya Nandi <soumyanandi2003@gmail.com>
2026-09-06 21:24:16 +0530

    Creating the route for note creation

---
+++ backend/src/routes/note.routes.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const noteRouter = express.Router();
+
+const noteController = require("../controllers/note.controller");
+const {isLoggedIn} = require("../middleware/auth");
+
+
+noteRouter.post("/:id/create-notes",isLoggedIn,noteController.handleCreateNote)
