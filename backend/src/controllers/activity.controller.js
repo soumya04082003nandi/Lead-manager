6d2cf78d3351724429c21dc166e84d5322fc5834
Soumya Nandi <soumyanandi2003@gmail.com>
2026-09-08 23:19:52 +0530

    Creating the controller for activity creation

--- backend/src/controllers/activity.controller.js
+++ backend/src/controllers/activity.controller.js
@@ -1,1 +1,32 @@
 const activityModel= require("../models/activityModel")
+
+
+const handleGetLeadActivities = async (req,res)=>{
+    try {
+
+        const {id}= req.params;
+
+        const activites= await activityModel
+        .find({lead:id})
+        .populate("User","name email role")
+        .sort({createdAt: -1});
+
+        return res.status(200).json({
+            success:true,
+            count:activites.length,
+            activites
+        })
+        
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error."
+        })
+    }
+}
+
+
+module.exports={
+    handleGetLeadActivities
+}
