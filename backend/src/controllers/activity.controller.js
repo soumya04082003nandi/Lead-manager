5249598aabd43a351c8346f76cc87892267fd6ad
Soumya Nandi <soumyanandi2003@gmail.com>
2026-09-12 00:16:32 +0530

    Validating the lead id in handleGetLeadActivities controller

--- backend/src/controllers/activity.controller.js
+++ backend/src/controllers/activity.controller.js
@@ -1,10 +1,18 @@
 const activityModel= require("../models/activityModel")
-
+const isValidObjectId= require("../utils/validateObjectId")
 
 const handleGetLeadActivities = async (req,res)=>{
     try {
 
         const {id}= req.params;
+
+        //Validating the id
+        if(!isValidObjectId(id)){
+            return res.status(400).json({
+                success:false,
+                message:"Invalid lead id."
+            })
+        };
 
         const activites= await activityModel
         .find({lead:id})
