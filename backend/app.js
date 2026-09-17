2375e0a5c169a1aa64ba4fe69757fa6ce7af4762
Soumya Nandi <soumyanandi2003@gmail.com>
2026-09-17 10:16:12 +0530

    Creating app.js and separeting server.js for testing

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const authRouter = require("./src/routes/auth.routes");
+const leadRouter = require("./src/routes/lead.routes");
+const noteRouter = require("./src/routes/note.routes");
+const activityRouter = require("./src/routes/activity.route");
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+// routes
+app.use("/api/auth", authRouter);
+app.use("/api/lead", leadRouter);
+app.use("/api/lead", noteRouter);
+app.use("/api/lead", activityRouter);
+
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,14 @@
-const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
-const connectDB = require("./src/config/db")
-const authRouter = require("./src/routes/auth.routes")
-const leadRouter = require("./src/routes/lead.routes")
-const noteRouter = require("./src/routes/note.routes")
-const activityRouter = require("./src/routes/activity.route");
-
-
+const connectDB = require("./src/config/db");
+const app = require("./app");
 
 dotenv.config();
 
-//database connection
+// database connection
 connectDB();
-
-const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-}));
-
-app.use(cookieParser());
-app.use(express.json());
-
-//routes
-app.use("/api/auth", authRouter);
-app.use("/api/lead",leadRouter)
-app.use("/api/lead",noteRouter)
-app.use("/api/lead", activityRouter);
-
-
-app.get("/", (req, res) => {
-    res.send("API is running");
-});
-
-
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
-
 });
