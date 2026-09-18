727b776bb25445cd84886951f2abf44a0ebe0d6c
Soumya Nandi <soumyanandi2003@gmail.com>
2026-09-19 00:15:38 +0530

    Accessing the route file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes/app.routes'
 
-function App() {
- return(
-  <h1>Starting buildin the frontend.</h1>
- )
+const App = ()=>{
+    return(
+
+        <BrowserRouter>
+            <AppRoutes/>
+        </BrowserRouter>
+    )
 }
 
 export default App
