93a6e0fa013439c49485006260980af7aa206033
Soumya Nandi <soumyanandi2003@gmail.com>
2026-09-19 10:12:57 +0530

    Linking the pages in route file

--- frontend/src/routes/app.routes.jsx
+++ frontend/src/routes/app.routes.jsx
@@ -1,12 +1,19 @@
 import {Routes ,Route} from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Leads from "../pages/Leads";
+import Dashboard from "../pages/Dashboard";
+import LeadDetails from "../pages/LeadDetails";
 
 
 const AppRoutes = ()=>{
     return(
         <Routes>
-            <Route path="/login" element={<h1>Login page</h1>} />
-            <Route path="/register" element= {<h1>Register page</h1>}></Route>
-            <Route path="/dashboard" element= {<h1>Dashboard page</h1>}></Route>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element= {<Register/>}></Route>
+            <Route path="/dashboard" element= {<Dashboard/>}></Route>
+            <Route path="/leads" element={<Leads/>}/>
+            <Route path="/leads/:id" element={<LeadDetails/>}></Route>
 
         </Routes>
     );
