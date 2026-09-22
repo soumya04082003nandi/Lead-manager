fce0c36c4dab04f0bbcd3d46d944a6d52df64b6f
Soumya Nandi <soumyanandi2003@gmail.com>
2026-09-22 23:47:30 +0530

    Accessing the authcontext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/app.routes'
+import {AuthProvider} from "./context/authContext"
 
 const App = ()=>{
     return(
 
         <BrowserRouter>
-            <AppRoutes/>
+            <AuthProvider>
+                <AppRoutes/>
+            </AuthProvider>
         </BrowserRouter>
     )
 }
