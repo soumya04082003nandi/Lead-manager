4a3b1f180183f28733c4fe91cddc255b9ba7b3c6
Soumya Nandi <soumyanandi2003@gmail.com>
2026-09-24 10:09:40 +0530

    Creating the  basic login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import {useState} from 'react'
+import {useNavigate} from "react-router-dom"
+import { useAuth } from '../hooks/useAuth'
+
 
 const Login = ()=>{
-    return(
-        <>
-            <h1>Login Page</h1>
-        </>
-    )
-}
+
+    const navigate = useNavigate();
+    const {login} = useAuth();
+
+    const [formData, setFormData] = useState({
+        email:"",
+        password:"",
+    });
+
+    const [error, setError]= useState("false");
+    const [loading, setLoading] = useState(false);
+    
+    const handleChange = (e)=>{
+        const {name, value}= e.target;
+
+        setFormData((prev)=>({
+            ...prev,
+            [name]:value,
+        }))
+    }
+
+    const handleSubmit = async (e)=>{
+        e.prventDefault();
+
+        setError("");
+        setLoading(true)
+
+        try {
+            await login(formData);
+
+            navigate("/dashboard")
+        } catch (error) {
+            setError(
+                error.response?.data?.message ||
+                "Login failed. Please check your credentials."
+            );
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            {error && <p>{error}</p>}
+
+            <form onSubmit={handleSubmit}>
+
+                <div>
+                    <label htmlFor="email">Email</label>
+
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password</label>
+
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <button type="submit" disabled={loading}>
+                    {loading ? "Logging in..." : "Login"}
+                </button>
+
+            </form>
+        </div>
+    );
+};
+
+
 
 export default Login
